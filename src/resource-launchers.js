90f58deb8c8a243f54042df2b64fcbee59edee0b
CSnhhsbs222 <topher_fuqua@outlook.com>
2026-07-09 07:01:37 -0500

    Connect Diagnosing with Confidence resources

--- src/resource-launchers.js
+++ src/resource-launchers.js
@@ -27,7 +27,13 @@ document.addEventListener('click', function (event) {
     'Service Flyer Option 3': 'assets/resources/Service Flyer (Option 3).pdf',
     'Telehealth 101': 'assets/resources/Telehealth 101.pptx',
     'Telehealth Procedures': 'assets/resources/Telehealth Procedures 5-2-24 Final.pdf',
-    'Telehealth Toybox': 'assets/resources/Telehealth toybox.docx'
+    'Telehealth Toybox': 'assets/resources/Telehealth toybox.docx',
+    'Diagnosing with Confidence': 'assets/resources/Diagnosing with Confidence.pptx',
+    'DSM-5-TR Simplified': 'assets/resources/DSMV Simplified.pdf',
+    'Episodes of Care FAQ': 'assets/resources/Episodes of Care - FAQ 3.2024.pdf',
+    'DSM-5-TR Guess Who Rulebook': 'assets/resources/DSM-5-TR Guess Who - Official Rulebook.docx',
+    'Printable Game Kit': 'assets/resources/DSM-5-TR Guess Who - Printable Game Kit.pptx',
+    'Symptom Cluster Quick Reference': 'assets/resources/DSM-5-TR Guess Who - Symptom Cluster Quick Reference Sheet.docx'
   };
 
   const resourceFile = resourceMap[button.textContent.trim()];
