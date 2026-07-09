ef685db7a344d842fc839e909e24132f1b809471
CSnhhsbs222 <topher_fuqua@outlook.com>
2026-07-09 01:15:26 -0500

    Connect Relationship Building resources

--- src/resource-launchers.js
+++ src/resource-launchers.js
@@ -20,7 +20,11 @@ document.addEventListener('click', function (event) {
     'School-Based Fillable Forms': 'assets/resources/School-based fillable forms.pdf',
     'Scheduling for Success': 'assets/resources/Scheduling for Success.pptx',
     'Excel Scheduler': 'assets/resources/Scheduler for Success.xlsx',
-    'District Calendars': 'assets/resources/Scheduler for Success.xlsx'
+    'District Calendars': 'assets/resources/Scheduler for Success.xlsx',
+    'Relationship Building': 'assets/resources/Relationship Building.pptx',
+    'Service Flyer Option 1': 'assets/resources/Service Flyer (Option 1).pdf',
+    'Service Flyer Option 2': 'assets/resources/Service Flyer (Option 2).pdf',
+    'Service Flyer Option 3': 'assets/resources/Service Flyer (Option 3).pdf'
   };
 
   const resourceFile = resourceMap[button.textContent.trim()];
